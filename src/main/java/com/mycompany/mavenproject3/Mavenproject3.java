0f188e48669c39ef3c1946dafc71220ebfb2e90c
Leirannt <93717185+Leirannt@users.noreply.github.com>
2024-06-13 22:23:10 +0700

    Update Mavenproject3.java
    tambah duplicate

--- src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
+++ src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
@@ -20,6 +20,10 @@ public class Mavenproject3 {
         return num1 - num2;
     }
 
+     public double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
     public double multiply(double num1, double num2) {
         return num1 * num2;
     }
